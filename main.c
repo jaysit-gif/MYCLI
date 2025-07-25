ce9788eb79a800bb598700ddcd950b39e015d414
D J <jayadithya.dhurwey@gmail.com>
2025-07-25 15:24:59 +0530

    second commit

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main(void){
     int a[3] = {11,12,2024};
     Task.completion = a;
     Task.status = true;
-    printf("%d",sizeof(task));
+    printf("%d\n",sizeof(task));
+    printf("%d\n",sizeof(Task));
     return 0;
 }
