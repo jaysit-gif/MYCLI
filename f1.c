4fee39daef9e9e3b133311d136fa3938f1f00ae8
D J <jayadithya.dhurwey@gmail.com>
2025-07-28 17:25:28 +0530

    new updates on f1

--- f1.c
+++ f1.c
@@ -47,16 +47,26 @@ char *inputline(void){
     return line;
 }
 
+int *inputdate(int *arr){
+    for(int i = 0;i<3;i++){
+        scanf("%d",&arr[i]);
+    }
+    return arr;
+}
+
 int main(void){
     task x;
     FILE *fp;
+    int ar[3];
     printf("ENTER: ");
     x.title = inputline();
+    printf("ENTER DATE IN DD-MM-YYYY: ");
+    x.completion = inputdate(ar);
     fp = fopen("F.txt","w");
     if(fp == NULL){
         return 1;
     }
-    fprintf(fp,"%s",x.title);
+    fprintf(fp,"%s\n %d %d %d\n",x.title,ar[0],ar[1],ar[2]);
     fclose(fp);
     return 0;
 }
